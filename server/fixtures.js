540e3f742869f0f0c1f23e4aa0758196ff63e911
Alice Yu <yaliceme@gmail.com>
2013-11-14 18:28:47 -0800

    Add fixture.js to pre-color facelets

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,4 +11,70 @@ if (Facelets.find().count() === 0) {
   Facelets.insert({_id:"lubb", color: "Blue"}); 
   Facelets.insert({_id:"ubb", color: "Blue"}); 
   Facelets.insert({_id:"rubb", color: "Blue"});
-}
+
+//Left starts as orange
+  Facelets.insert({_id:"bdll", color: "Orange"}); 
+  Facelets.insert({_id:"bll", color: "Orange"}); 
+  Facelets.insert({_id:"bull", color: "Orange"}); 
+
+  Facelets.insert({_id:"dll", color: "Orange"}); 
+  Facelets.insert({_id:"l", color: "Orange"}); 
+  Facelets.insert({_id:"ull", color: "Orange"}); 
+
+  Facelets.insert({_id:"dfll", color: "Orange"}); 
+  Facelets.insert({_id:"fll", color: "Orange"}); 
+  Facelets.insert({_id:"full", color: "Orange"}); 
+
+//Up starts as white
+  Facelets.insert({_id:"lbuu", color: "White"});
+  Facelets.insert({_id:"buu", color: "White"});
+  Facelets.insert({_id:"rbuu", color: "White"}); 
+
+  Facelets.insert({_id:"luu", color: "White"}); 
+  Facelets.insert({_id:"u", color: "White"}); 
+  Facelets.insert({_id:"ruu", color: "White"}); 
+
+  Facelets.insert({_id:"lfuu", color: "White"}); 
+  Facelets.insert({_id:"fuu", color: "White"}); 
+  Facelets.insert({_id:"fruu", color: "White"}); 
+
+//Right starts as red
+  Facelets.insert({_id:"burr", color: "Red"});
+  Facelets.insert({_id:"brr", color: "Red"});
+  Facelets.insert({_id:"bdrr", color: "Red"});
+
+  Facelets.insert({_id:"urr", color: "Red"});
+  Facelets.insert({_id:"r", color: "Red"});
+  Facelets.insert({_id:"drr", color: "Red"});
+
+  Facelets.insert({_id:"furr", color: "Red"});
+  Facelets.insert({_id:"frr", color: "Red"});
+  Facelets.insert({_id:"fdrr", color: "Red"});
+
+//Down starts as yellow
+  Facelets.insert({_id:"rbdd", color: "Yellow"});
+  Facelets.insert({_id:"bdd", color: "Yellow"});
+  Facelets.insert({_id:"bldd", color: "Yellow"});
+
+  Facelets.insert({_id:"rdd", color: "Yellow"});
+  Facelets.insert({_id:"d", color: "Yellow"});
+  Facelets.insert({_id:"ldd", color: "Yellow"});
+
+  Facelets.insert({_id:"frdd", color: "Yellow"});
+  Facelets.insert({_id:"fdd", color: "Yellow"});
+  Facelets.insert({_id:"lfdd", color: "Yellow"});
+
+//Front starts as green
+  Facelets.insert({_id:"luff", color: "Green"});
+  Facelets.insert({_id:"uff", color: "Green"});
+  Facelets.insert({_id:"ruff", color: "Green"});
+
+  Facelets.insert({_id:"lff", color: "Green"});
+  Facelets.insert({_id:"f", color: "Green"});
+  Facelets.insert({_id:"rff", color: "Green"});
+
+  Facelets.insert({_id:"dlff", color: "Green"});
+  Facelets.insert({_id:"dff", color: "Green"});
+  Facelets.insert({_id:"drff", color: "Green"});
+
+};
